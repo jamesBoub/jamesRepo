ab666e394e6d1fd5c9036900c8fafeb1b1a6bcc5
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2024-01-11 03:04:05 -0700

    Update main.lua
    removes the projectile trail. When a projectile's x and y velocities cross a set threshold they stop moving and remain rendered instead of being deleted.

--- lua/tankGame/main.lua
+++ lua/tankGame/main.lua
@@ -56,21 +56,18 @@ function power_gauge()
 end
 
 function projectile_create()
-    table.insert(projectiles, {x = (tank.turretX + math.cos(tank.turretAngle)*20) - 3, y = (tank.turretY - math.sin(tank.turretAngle)*-20) - 4, anglex = math.cos(tank.turretAngle)*game.projectileSpeed, angley = math.sin(tank.turretAngle)*game.projectileSpeed, gravity = 0})
+    table.insert(projectiles, {x = (tank.turretX + math.cos(tank.turretAngle)*20) - 3, y = (tank.turretY - math.sin(tank.turretAngle)*-20) - 4, anglex = math.cos(tank.turretAngle)*game.projectileSpeed, angley = math.sin(tank.turretAngle)*game.projectileSpeed, gravity = 0, moving = true})
 end
 
 function projectile_move()
   for i in pairs(projectiles) do
-      projectiles[i].x = projectiles[i].x + projectiles[i].anglex 
-      projectiles[i].y = projectiles[i].y - projectiles[i].angley * -1
-      
-      projectiles[i].y = projectiles[i].y + projectiles[i].gravity
-      projectiles[i].gravity = projectiles[i].gravity + game.gravity
-      
-      
-      
-      
-      
+    if projectiles[i].moving then
+        projectiles[i].x = projectiles[i].x + projectiles[i].anglex 
+        projectiles[i].y = projectiles[i].y - projectiles[i].angley * -1
+        
+        projectiles[i].y = projectiles[i].y + projectiles[i].gravity
+        projectiles[i].gravity = projectiles[i].gravity + game.gravity
+      end
   end
 end
 
@@ -125,10 +122,10 @@ function ground_render()
     end
 end
 
-function game_render()
-  tank_render()
-  projectile_move()
-end
+--function game_render()
+--  tank_render()
+--  projectile_move()
+--end
 
 function projectile_camera_follow()
   love.graphics.push()
@@ -139,7 +136,7 @@ function projectile_camera_follow()
     
   love.graphics.push()
     love.graphics.translate(projectiles[#projectiles].x * -1 + tank.turretX, 0)
-    love.graphics.translate(-tank.x + 200,0)
+    love.graphics.translate(-tank.x + 240,0)
     projectile_render()
     projectileTrailDraw()
     tank_render()
@@ -165,13 +162,16 @@ function projectile_collisions()
   for i in pairs(projectiles) do
       if projectiles[i].y + 6 >= 520 then
 --          table.remove(projectiles, i)
+
+
+if projectiles[i].anglex < .5 and projectiles[i].angley < 5 and projectiles[i].anglex > 0 or projectiles[i].anglex > -.5 and projectiles[i].angley > - .5 and projectiles[i].anglex < 0 then
+--    table.remove(projectiles, i)
+projectiles[i].moving = false
+else
 projectiles[i].y = 514
-projectiles[i].gravity = 0
+projectiles[i].gravity = 0 
 projectiles[i].anglex = projectiles[i].anglex * .7
 projectiles[i].angley = projectiles[i].angley * .7
-
-if projectiles[i].anglex < 1 and projectiles[i].angley < 1 then
-    table.remove(projectiles, i)
 end
 
 --projectiles[i].anglex = projectiles[i].anglex * 1
@@ -184,7 +184,7 @@ function projectileTrailDraw()
   if timer <= 0 then
     for i in pairs(projectiles) do
           timer = 1
-          table.insert(projectileTrails, {x = projectiles[i].x, y = projectiles[i].y})
+--          table.insert(projectileTrails, {x = projectiles[i].x, y = projectiles[i].y})
     end
   end
   
