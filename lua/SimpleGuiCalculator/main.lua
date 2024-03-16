38ba0d5c10ada3e78c18aaad8e4ea6ea616c0ea3
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2024-03-16 03:58:59 -0700

    Update main.lua

--- lua/SimpleGuiCalculator/main.lua
+++ lua/SimpleGuiCalculator/main.lua
@@ -15,12 +15,18 @@ function buttion_create()
   _y = 50
   _w = 50
   _h = 50
+  num = 0
   
   for x = 1,5 do
     _x = 0
     _y = _y + _h + 5
     for z = 1,5 do
-      table.insert(buttons, {x = _x, y = _y, w = _w, h = _h, text = "ass"})
+      if x <= 2 then
+      table.insert(buttons, {x = _x, y = _y, w = _w, h = _h, text = num, keyFunc = num})
+      num = num + 1
+    else
+      table.insert(buttons, {x = _x, y = _y, w = _w, h = _h, text = "butt", keyFunc = num})
+    end
       _x = _x + _w + 5
     end
   end
@@ -32,7 +38,7 @@ function button_draw()
       love.graphics.rectangle("fill", buttons[i].x, buttons[i].y, buttons[i].w, buttons[i].h)
       if buttons[i].text ~= nil then
         love.graphics.setColor(1,0,0)
-        love.graphics.print(buttons[i].text, buttons[i].x, buttons[i].y)
+        love.graphics.print(buttons[i].text, buttons[i].x + buttons[i].w / 2, buttons[i].y + buttons[i].w / 1.5)
       end
   end
 end
