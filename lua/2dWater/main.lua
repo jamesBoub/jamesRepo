ab695e136df9c218c22ae0939e7bd35ce3169688
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2024-05-01 17:56:49 -0700

    Add files via upload

---
+++ lua/2dWater/main.lua
@@ -0,0 +1,80 @@
+function love.load()
+  blocks = {}
+  grid_generate()
+  
+  
+  for i in pairs(grid) do
+      if grid[i].x / 21 == 5 and grid[i].y / 21 == 1 then 
+          grid[i].status.block = true
+        end
+    end
+  
+  
+--  table.insert(blocks, {x = 2, y = 9})
+--  table.insert(blocks, {x = 5, y = 1})
+end
+
+function love.update()
+  
+end
+--                    table.insert(blocks, {x = grid[collidedCell].x / 21, y = grid[collidedCell].y / 21})
+
+function love.mousereleased(x,y,button)
+  local xPos, yPos = love.mouse.getPosition()
+  if button == 1 then
+      if mouse_grid_collision_check() then
+        for i in pairs(grid) do
+            if xPos > grid[i].x and xPos < grid[i].x + grid[i].w and yPos > grid[i].y and yPos < grid[i].y + grid[i].w and grid[i].status.block then
+--              love.event.quit()
+              grid[i].status.block = false
+            elseif xPos > grid[i].x and xPos < grid[i].x + grid[i].w and yPos > grid[i].y and yPos < grid[i].y + grid[i].w and grid[i].status.block == false then
+              grid[i].status.block = true
+              end
+          end
+        print(xPos, yPos)
+        end
+    end
+end
+
+function love.draw()
+  for i in pairs(grid) do
+      love.graphics.setColor(1,1,1)
+        if grid[i].status.block then
+          love.graphics.setColor(1,0,0)
+        end
+      love.graphics.rectangle('fill', grid[i].x, grid[i].y, grid[i].l, grid[i].w)
+    end
+end
+function circle_draw()
+  
+end
+function mouse_grid_collision_check()
+  local mouseX, mouseY = love.mouse.getPosition()
+  for i in pairs(grid) do
+    if mouseX > grid[i].x and mouseX < grid[i].x + grid[i].w and mouseY > grid[i].y and mouseY < grid[i].y + grid[i].l then
+        collidedCell = i
+        return true, collidedCell
+      end
+  end
+end
+
+function grid_generate()
+  grid = {}
+  local _x = 0
+  local _y = 0
+  local gridWidth = 20
+  local gridHeight = 20
+  
+  for x = 1,gridHeight do
+    _y = _y + 21
+    _x = 0
+    for y = 1,gridWidth do
+      table.insert(grid, {x = _x, y = _y, w = 20, l = 20, status = {
+            block = false
+          }})
+      
+      _x = _x + 21
+    end
+  end
+end
+
