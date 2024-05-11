88c7124f2e6722a8a74df634ef64f300120ede89
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2024-05-10 17:32:01 -0700

    Update main.lua

--- lua/2dWater/main.lua
+++ lua/2dWater/main.lua
@@ -1,78 +1,65 @@
 function love.load()
-  blocks = {}
-  grid_generate()
-  
-  
-  for i in pairs(grid) do
-      if grid[i].x / 21 == 5 and grid[i].y / 21 == 1 then 
-          grid[i].status.block = true
-        end
-    end
-  
-  
---  table.insert(blocks, {x = 2, y = 9})
---  table.insert(blocks, {x = 5, y = 1})
+  grid_create()
 end
 
 function love.update()
-end
---                    table.insert(blocks, {x = grid[collidedCell].x / 21, y = grid[collidedCell].y / 21})
-
-
-function love.mousereleased(x,y,button)
-  local xPos, yPos = love.mouse.getPosition()
-  if button == 1 then
-      if mouse_grid_collision_check() then
-        print(collidedCell)
-            if grid[collidedCell].status.block then
---              love.event.quit()
-              grid[collidedCell].status.block = false
-            elseif  grid[collidedCell].status.block == false then
-              grid[collidedCell].status.block = true
-              end
---        print(xPos, yPos)
-        end
-    end
+  mouse_grid_collide()
 end
 
 function love.draw()
-  for i in pairs(grid) do
-      love.graphics.setColor(1,1,1)
-        if grid[i].status.block then
-          love.graphics.setColor(1,0,0)
-        end
-      love.graphics.rectangle('fill', grid[i].x, grid[i].y, grid[i].l, grid[i].w)
-    end
+  grid_render()
 end
-function circle_draw()
-  
-end
-function mouse_grid_collision_check()
-  local mouseX, mouseY = love.mouse.getPosition()
-  for i in pairs(grid) do
-    if mouseX > grid[i].x and mouseX < grid[i].x + grid[i].w and mouseY > grid[i].y and mouseY < grid[i].y + grid[i].l then
-        collidedCell = i
-        return true, collidedCell, grid[i].status
+
+function grid_create()
+  grid = {}
+  blocks = {}
+  _x = 0
+  _y = 0
+  for y = 1,40 do
+    _y = _y + 10
+    _x = 10
+    for x = 1,40 do
+      table.insert(grid, {x = _x, y = _y})
+      _x = _x + 10
       end
   end
 end
-
-function grid_generate()
-  grid = {}
-  local _x = 0
-  local _y = 0
-  local gridWidth = 20
-  local gridHeight = 20
+ 
+function grid_render()
   
-  for x = 1,gridHeight do
-    _y = _y + 21
-    _x = 0
-    for y = 1,gridWidth do
-      table.insert(grid, {x = _x, y = _y, w = 20, l = 20, status = {
-            block = false
-          }})
-      
-      _x = _x + 21
+  
+  
+  for i in pairs(grid) do
+      love.graphics.setColor(1,1,1)
+    
+    for u in pairs(blocks) do
+        if blocks[u][1].x == grid[i].x and blocks[u][1].y == grid[i].y then
+          love.graphics.setColor(1,0,0)
+          end
+      end
+    
+    love.graphics.rectangle("fill", grid[i].x, grid[i].y, 9, 9)
+  end
+end
+
+function mouse_grid_collide()
+  mouseX,mouseY = love.mouse.getPosition()
+    for i in pairs(grid) do
+        if mouseX > grid[i].x and mouseX < grid[i].x + 9 and mouseY > grid[i].y and mouseY < grid[i].y + 9 then
+          gridCollidedWith = i
+          return true, gridCollidedWith
+        end
     end
+end
+
+function love.keyreleased(key)
+end
+
+function love.mousereleased(x,y,button)
+  if button == 1 and mouse_grid_collide() then
+    table.insert(blocks, {})
+    table.insert(blocks[#blocks], {x = grid[gridCollidedWith].x, y = grid[gridCollidedWith].y})
+--    print(blocks[#blocks].x / 10 .. " " .. blocks[#blocks].y / 10)
+print(blocks[#blocks][1].x)
   end
 end
