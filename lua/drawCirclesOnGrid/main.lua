afc78777aabb8974b9bb16ee3f261a0de4797e37
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2024-06-14 16:17:55 -0700

    Update main.lua

--- lua/drawCirclesOnGrid/main.lua
+++ lua/drawCirclesOnGrid/main.lua
@@ -2,6 +2,7 @@ function love.load()
   circleX = 200
   circleY = 200
   circleR = 100
+  circleDim = 5
   
   grid_generate()
 end
@@ -21,10 +22,10 @@ function love.draw()
 
   for i in pairs(grid) do
             love.graphics.setColor(grid[i].color)
-            love.graphics.rectangle("fill", grid[i].x, grid[i].y, 10, 10)
+            love.graphics.rectangle("fill", grid[i].x, grid[i].y, circleDim, circleDim)
 
 love.graphics.setColor(1,1,1)
-  love.graphics.circle("fill", circleX, circleY, circleR)
+--  love.graphics.circle("fill", circleX, circleY, circleR)
 
 
     if  distance_between_2_points(grid[i].x + 11,circleX,grid[i].y,circleY) < circleR or distance_between_2_points(grid[i].x ,circleX,grid[i].y + 11,circleY) < circleR or distance_between_2_points(grid[i].x + 11 ,circleX,grid[i].y + 11,circleY) < circleR or distance_between_2_points(grid[i].x ,circleX,grid[i].y,circleY) < circleR then
@@ -56,12 +57,12 @@ function grid_generate()
   local xOff = 0
   local yOff = 0
   
-  for x = 1,50 do
-    yOff = yOff + 11
+  for x = 1,100 do
+    yOff = yOff + circleDim + 1
     xOff = 0
-  for y = 1,50 do
+  for y = 1,100 do
     table.insert(grid, {x = xOff, y = yOff, color = {1,0,1}})
-xOff = xOff + 11
+xOff = xOff + circleDim + 1
   end
     end
   
