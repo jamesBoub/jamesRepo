e0482862efbec6f999c6380caf8916fbce6ceca8
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2024-07-25 00:39:38 -0700

    Update main.lua

--- lua/graphQuadratic/main.lua
+++ lua/graphQuadratic/main.lua
@@ -2,14 +2,28 @@ function love.load()
 
 a = 1
 b = 0
-c = -5
+c = -4
+width,height = love.graphics.getDimensions()
+
+gridLines = {}
+xoff = 0
+xoff2 = 0
+for x = 1,40 do
+   
+    table.insert(gridLines, {x1 = xoff,y1 = 0,x2 = xoff,y2 = 800})
+    xoff = xoff + 20
+--    print(xoff)
+for y = 1,40 do
+    table.insert(gridLines, {x1 = 0,y1 = xoff2,x2 = 800,y2 = xoff2})
+    xoff2 = xoff2 + 20
+  end
+  end
 
 xInt1 = -b+math.sqrt(((b^2)-4*a*c))/(2*a)
 xInt2 = -b-math.sqrt(((b^2)-4*a*c))/(2*a)
 
-print(xInt1)
+--print(xInt1)
 
-width,height = love.graphics.getDimensions()
 
 xPos = width/2
 yPos = height/2
@@ -19,6 +33,10 @@ function love.draw()
   love.graphics.setColor(1,1,1)
   love.graphics.line(0, height/2, width, height/2)
   love.graphics.line(width/2,0, width/2, height)
+--love.graphics.line(0,10,100,10)
+  for i in pairs(gridLines) do
+    love.graphics.line(gridLines[i].x1, gridLines[i].y1, gridLines[i].x2, gridLines[i].y2)
+  end
   
   love.graphics.setColor(1,0,0)
   
@@ -29,7 +47,7 @@ function love.draw()
   end
   
   love.graphics.translate(xPos, yPos)
-  
+--  print(xPos)
    lastX, lastY = nil
   for x = -100,100 do
     local y = -(a * (x/20)^2 + b * (x/20) + c)
