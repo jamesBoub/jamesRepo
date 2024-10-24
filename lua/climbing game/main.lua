a50dd051f19faab474a48dd78b1960d11a669b54
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2024-10-23 23:00:11 -0700

    Update main.lua

--- lua/climbing game/main.lua
+++ lua/climbing game/main.lua
@@ -1,8 +1,12 @@
-player = {x = 50, y = 100, w = 10, h = 10, v = 2}
+player = {x = 50, y = 100, w = 10, h = 10, v = 2, collision = false}
 obstacles = {}
+falling = true
 
 function love.draw()
   input()
+  playerGravity()
+  
+  
   
   if collisions(player,obstacles) then
     resolveCollision(player, obstacles)
@@ -17,12 +21,17 @@ function love.draw()
   
   love.graphics.setColor(1,1,1)
   love.graphics.rectangle('fill', player.x, player.y, player.w, player.h)
+  
+  
+  
+  
 end
 
 table.insert(obstacles, {x = 50, y = 150, w = 50, h = 50})
 table.insert(obstacles, {x = 110, y = 150, w = 50, h = 50})
 
 function input()
+  player.collision = false
   if love.keyboard.isDown("w") then
     player.y = player.y - player.v
   end
@@ -37,6 +46,20 @@ function input()
   end
 end
 
+function playerGravity()
+  if falling then
+    player.y = player.y + 1
+  end
+  
+  if player.collision then
+    falling = false
+  elseif player.collision == false then
+    falling = true
+  end
+  
+end
+
+
 function collisions(a,b)
   for i in pairs(obstacles) do
     if a.x + a.w > b[i].x and a.x < b[i].x + b[i].w and a.y + a.h > b[i].y and a.y < b[i].y + b[i].h then
@@ -48,10 +71,9 @@ end
 function resolveCollision(player, obstacles)
     for i in pairs(obstacles) do
       
-      if player.x + player.w > obstacles[i].x and player.x < obstacles[i].x + obstacles[i].w and player.y + player.h > obstacles[i].y and player.y < obstacles[i].y + obstacles[i].h then
+      if player.x + player.w >= obstacles[i].x and player.x <= obstacles[i].x + obstacles[i].w and player.y + player.h >= obstacles[i].y and player.y <= obstacles[i].y + obstacles[i].h then
       
-        local t = obstacles[i]
-        
+          local t = obstacles[i]
            
           local overlapX = (player.x + player.w) - t.x -- left collision check
           local overlapX2 = (t.x + t.w) - player.x -- right collision check
@@ -70,14 +92,19 @@ function resolveCollision(player, obstacles)
             
             if FinalxOverlap < FinalyOverlap then
                 if overlapX < overlapX2 then
-                    player.x = player.x - FinalxOverlap  
+                    -- hit right
+                    player.x = player.x - FinalxOverlap
                 else
+                    -- hit left
                     player.x = player.x + overlapX2  
                 end
             else
                 if overlapY < overlapY2 then
+                    -- hit top
                     player.y = player.y - FinalyOverlap  
+                    player.collision = true
                 else
+                    -- hit bottom
                     player.y = player.y + overlapY2  
                 end
               end
