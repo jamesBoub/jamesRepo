4e0d7cd9097a40a74be0202c495fe662cf51c83a
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2025-01-17 15:57:30 -0700

    Update five.cpp

--- cpp/createGrid02/five/five.cpp
+++ cpp/createGrid02/five/five.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main() {
+
+	struct Pos {
+		int x;
+		int y;
+	};
 	
 	int rows = 5;
 	int columns = 5;
@@ -10,20 +15,21 @@ int main() {
 	int gridX = 0;
 	int gridY = 0;	
 
-	vector<vector<int>> grid(rows, vector<int>(columns, 0));
+	vector<vector<Pos>> grid(rows, vector<Pos>(columns));
 
 	for (int x = 0; x < rows; x++) {
 		gridY = 0;
 		gridX = gridX + 1;
 		for (int y = 0; y < rows; y++) {
 			gridY = gridY + 1;
-			grid[x][y] = gridX * 10 + gridY;
+			grid[x][y].x = gridX;
+			grid[x][y].y = gridY;
 		}	
 	}
 
 for (int x = 0; x < rows; x++) {
 	for (int y = 0; y < columns; y++) {
-	cout<<grid[x][y] << " ";
+	cout<<"[ "<<grid[x][y].y<<" "<<grid[x][y].x<<" ]";	
 	}
 	cout<<endl;
 }
