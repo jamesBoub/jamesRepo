91fb75f1bd7111aa3e1a1546b91c13a38355b817
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2025-06-24 11:10:15 -0700

    Update main.lua

--- lua/circleDrawMouseFollow/main.lua
+++ lua/circleDrawMouseFollow/main.lua
@@ -1,19 +1,20 @@
 rays = {
-
-origin = {
-			x = 200,
-			y = 200,
-			r = 100,
-			a = math.rad(0)
-		 },
+	origin = {
+				x = 200,
+				y = 200,
+				r = 100,
+				a = math.rad(0)
+			 },
 lines = {}
 }
 
-function create_lines()
+lineNumSel = 10
+
+function create_lines(numberOfLines)
 	local angle = 0
 
 	while angle < math.rad(360) do
-		angle = angle + math.rad(10)
+		angle = angle + math.rad(numberOfLines)
 		
 		_x = rays.origin.x + math.cos(angle) * rays.origin.r
 		_y = rays.origin.y + math.sin(angle) * rays.origin.r
@@ -35,19 +36,35 @@ rays.origin.y = mouseY
 	end
 end
 
-create_lines()
+function ray_clear()
+	for i in pairs(rays.lines) do
+		rays.lines[i] = nil
+	end
+end
 
 function love.draw()
-
 mouseX, mouseY = love.mouse.getPosition()
+
+rays_update()
 
 	for i in pairs(rays.lines) do
 		love.graphics.line(rays.origin.x, rays.origin.y, rays.lines[i].x, rays.lines[i].y)
 	end
 	
-	rays_update()
-	
+	function love.keyreleased(key) 
+		if key == "up" then
+			lineNumSel = lineNumSel + 1
+			ray_clear()
+			create_lines(lineNumSel)
+		elseif key == "down" then
+			if lineNumSel > 1 then
+				lineNumSel = lineNumSel - 1
+				ray_clear()
+				create_lines(lineNumSel)
+			end
+		end
+	end
 end
 
+create_lines(10)
 print(rays.lines[1].a .. " " .. rays.lines[5].a)
-
