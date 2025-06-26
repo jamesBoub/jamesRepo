505c70595b2e630aeb47b3d697fa3a82851ac4ab
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2025-06-26 01:33:19 -0700

    Update main.lua

--- lua/lineLineCollisionDetect/main.lua
+++ lua/lineLineCollisionDetect/main.lua
@@ -1,6 +1,8 @@
 line = {x1 = 0, x2 = 0, y1 = 0, y2 = 0}
 rectangle = {x = 100, y = 100, w = 50, h = 50}
 
+manyLines = {}
+
 function love.draw()
 
 	mouseX, mouseY = love.mouse.getPosition()
@@ -11,7 +13,12 @@ function love.draw()
 	
 	love.graphics.setColor(1,1,1)
 	--~ love.graphics.rectangle("fill", rectangle.x, rectangle.y, rectangle.w, rectangle.h)
-	love.graphics.line(50, 100, 500, 250)
+	--~ love.graphics.line(50, 100, 500, 250)
+	
+	for i in pairs(manyLines) do
+		love.graphics.line(manyLines[i].x1, manyLines[i].y1, manyLines[i].x2, manyLines[i].y2)
+		line_line_intersection(manyLines[i].x1, manyLines[i].x2, line.x1, line.x2, manyLines[i].y1, manyLines[i].y2, line.y1, line.y2)
+	end
 	
 	love.graphics.setColor(1,0,0)
 	love.graphics.line(line.x1, line.y1, line.x2, line.y2)
@@ -19,13 +26,13 @@ function love.draw()
 	
 	player_input()
 	
-	line_line_intersection(50, 500, line.x1, line.x2, 100, 250, line.y1, line.y2)
+	--~ line_line_intersection(50, 500, line.x1, line.x2, 100, 250, line.y1, line.y2)
 	
 
 	
 end
 
-function line_line_intersection(x1, x2, x3, x4, y1, y2, y3, y4)
+function line_line_intersection(x1, x2, x3, x4, y1, y2, y3, y4) -- 4 points for line a and b
 	local u_a = (((x4 - x3) * (y1 - y3)) - ((y4 - y3) * (x1 - x3))) / (((y4 - y3) * (x2 - x1)) - (x4 - x3) * (y2 - y1))
 	local u_b = (((x2 - x1) * (y1 - y3)) - ((y2 - y1) * (x1 - x3))) / (((y4 - y3) * (x2 - x1)) - (x4 - x3) * (y2 - y1))
 	
@@ -35,6 +42,17 @@ function line_line_intersection(x1, x2, x3, x4, y1, y2, y3, y4)
 		
 		love.graphics.circle("fill", intersectX, intersectY, 4)
 		
+	end
+end
+
+function rand_line_creator(numLines)
+	for _ = 1,numLines do
+		local randX1 = love.math.random(1,600)
+		local randX2 = love.math.random(1,600)
+		local randY1 = love.math.random(1,800)
+		local randY2 = love.math.random(1,800)
+		
+		table.insert(manyLines, {x1 = randX1, y1 = randY1, x2 = randX2, y2 = randY2})
 	end
 end
 
@@ -49,3 +67,5 @@ function player_input()
 		line.x1 = line.x1 + 1
 	end
 end
+
+rand_line_creator(5)
