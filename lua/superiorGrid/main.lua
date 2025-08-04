6751b5b8bcf6b3e827ec9168ba4e0e87cf666d65
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2025-08-04 01:31:55 -0700

    Update main.lua

--- lua/superiorGrid/main.lua
+++ lua/superiorGrid/main.lua
@@ -1,18 +1,35 @@
-player = {}
+player = {x = 1, y = 1}
 world = {}
+
+gridW = 40
+gridH = 40
 
 function love.draw()
 	for i in pairs(world) do
 		for u in pairs(world[i]) do
-			love.graphics.rectangle("fill", world[i][u].x, world[i][u].y, 10,10)
+			if world[i][u].id == "player" then
+				love.graphics.setColor(1,0,0)
+			elseif world[i][u].flags == "solid" then
+				love.graphics.setColor(0,0,1)
+			else
+				love.graphics.setColor(1,1,1)
+			end
+				love.graphics.rectangle("fill", world[i][u].x, world[i][u].y, 10,10)
+			end
 		end
+	love.graphics.print(player.x .. " " .. player.y, 500,0)
+	textbox_render()
+end
+
+function love.update(dt)
+	player_input()
+	if player_on_grid(40,40) then
+		world[player.x][player.y].id = "player"
 	end
 end
 
-function love.update()
-end
-
 function grid_create(rows,cols)
+	-- creates the grid and cells
 local xOff = 0
 local yOff = 0
 	for _x = 1, rows do
@@ -21,12 +38,139 @@ local yOff = 0
 		xOff = xOff + 11
 		for _y = 1, cols do
 			world[_x][_y] = {}	
-			world[_x][_y] = {x = xOff, y = yOff}
+			world[_x][_y] = {x = xOff, y = yOff, id = {}, flags = {}}
 			yOff = yOff + 11
+		end
+	end
+end
+
+function player_input()
+	function love.keyreleased(key)
+		if key == "w" then
+			player_move("up")
+		elseif key == "a" then
+			player_move("left")
+		elseif key == "s" then
+			player_move("down")
+		elseif key == "d" then
+			player_move("right")
+		end
+	end
+	
+	
+	function love.mousereleased(x,y,key)
+		if key == 1 then
+			local mouseOnCell, cellReturned = mouse_is_on_cell()
+				if mouseOnCell then
+					cell_modify(cellReturned,"solid")
+				end
+		elseif key == 2 then
+			local mouseOnCell, cellReturned = mouse_is_on_cell()
+				if mouseOnCell then
+					cell_modify(cellReturned)
+				end
+		end
+	end
+end
+
+function player_on_grid(w,h)
+	-- checks whether the player is on the grid or not
+	if player.x >= 1 and player.x <= 40 and player.y >= 1 and player.y <= 40 then
+		return true
+	end
+end
+
+function player_move(direction)
+	world[player.x][player.y].id = {}
+	if direction == "up" then
+		if player.y > 1 and cell_is_free(world[player.x][player.y - 1]) then
+		player.y = player.y - 1
+		push_text("moved up" .. " " .. textbox.current, true)
+		end
+	elseif direction == "down" then
+		if player.y < gridH  and cell_is_free(world[player.x][player.y + 1])  then
+		player.y = player.y + 1
+		push_text("moved down" .. " " .. textbox.current, true)
+		end
+	elseif direction == "left" then
+		if player.x > 1  and cell_is_free(world[player.x - 1][player.y])  then
+		player.x = player.x - 1
+		push_text("moved left" .. " " .. textbox.current, true)
+		end
+	elseif direction == "right" then
+		if player.x < gridW and cell_is_free(world[player.x + 1][player.y])  then
+		player.x = player.x + 1
+		push_text("moved right" .. " " .. textbox.current, true)
+		end
+	end
+end
+
+function cell_is_free(cell)
+ -- checks whether the cell is unoccupied
+	if cell.flags == "solid" then
+		return false
+	else
+		return true
+	end
+	--~ return true
+end
+
+function mouse_is_on_cell()
+
+	local mouseX, mouseY = love.mouse.getPosition()
+
+	for i in pairs(world) do
+		for u in pairs(world[i]) do
+			if mouseX > world[i][u].x and mouseX < world[i][u].x + 10 and mouseY > world[i][u].y and mouseY < world[i][u].y + 10 then
+				local cellRolledOver = world[i][u]
+				return true, cellRolledOver
 			end
 		end
 	end
+end
 
-grid_create(40,40)
-print(world[1][2].x)
+function cell_modify(cell,modification)
+	-- changes a cell based on modification. leave modification empty to remove flag.
+	cell.flags = modification
+	print(cell.flags)
+end
+
+function create_textbox()
+	textbox = {
+		current = 1,
+		lines = {
+			{words = "",x = 600, y = 0},
+			{words = "",x = 600, y = 25},
+			{words = "",x = 600, y = 50},
+			{words = "",x = 600, y = 75},
+			{words = "",x = 600, y = 100},
+			{words = "",x = 600, y = 125},
+			{words = "",x = 600, y = 150},
+			{words = "",x = 600, y = 175}
+		}
+	}
+	
+end
+
+function textbox()
+	function push_text(message,next)
+		for _x = 1,7 do
+			textbox.lines[_x].words = textbox.lines[_x + 1].words
+		end
+		textbox.lines[8].words = message
+	end
+		function textbox_render()
+			for i in pairs(textbox.lines) do
+				love.graphics.print(textbox.lines[i].words, textbox.lines[i].x, textbox.lines[i].y)
+			end
+	end
+end
+
+textbox()
+grid_create(gridW,gridH)
+create_textbox()
+
+
+print(textbox.lines[1].x)
+print(textbox.lines[2].words)
 
