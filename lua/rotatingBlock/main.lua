4e58a89f30b3aab59638c6dcc367444e686b02d4
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2026-01-12 05:47:10 -0700

    Update main.lua

--- lua/rotatingBlock/main.lua
+++ lua/rotatingBlock/main.lua
@@ -1,3 +1,4 @@
+
 --~ math.randomseed(os.time())
 
 target = {}
@@ -9,17 +10,32 @@ ring = {}
 --~ ring.y = math.random(600)
 
 theta = 0
+radiusX = 100
+radiusY = 100
+angleOffset = 0
+
+for i = 1,16 do
+		--~ ring[1].x = target.x + math.cos(theta) * 100
+		--~ ring[1].y = target.y + math.sin(theta) * 100
+		
+		table.insert(ring, {x = target.x + math.cos(theta + angleOffset) * radiusX, y = target.y + math.sin(theta + angleOffset) * radiusY, angle = theta + angleOffset})
+		angleOffset = angleOffset + .4
+end
+
 
 function love.draw()
-
-	ring.x = target.x + math.cos(theta) * 100
-	ring.y = target.y + math.sin(theta) * 100
-
+	
 	love.graphics.setColor(1,1,1)
 	love.graphics.rectangle("fill", target.x, target.y, 10,10)
-	
 	love.graphics.setColor(1,0,0)
-	love.graphics.rectangle("fill", ring.x, ring.y, 10,10)
+	
+	for i in pairs(ring) do
+		ring[i].x = target.x + math.cos(ring[i].angle) * radiusX
+		ring[i].y = target.y + math.sin(ring[i].angle) * radiusY
+		love.graphics.rectangle("fill", ring[i].x, ring[i].y, 10,10)
+	end
+	
+	print(ring[1].x)
 	
 	love.graphics.print(math.deg(theta))
 	
@@ -33,10 +49,38 @@ function love.draw()
 		target.x = target.x + 5
 	end
 	
-	if love.keyboard.isDown("up") then
-		theta = theta + .1
-	elseif love.keyboard.isDown("down") then
-		theta = theta - .1
+	if love.keyboard.isDown("lctrl") then
+		for i in pairs(ring) do
+			ring[i].angle = ring[i].angle + .02
+			ring[i].x = target.x + math.cos(ring[i].angle) * radiusX
+			ring[i].y = target.y + math.sin(ring[i].angle) * radiusY
+		end
 	end
+	
+	if love.keyboard.isDown("up") then
+		radiusX = radiusX + 2
+		radiusY = radiusY + 2
+	elseif love.keyboard.isDown("down") then
+		radiusX = radiusX - 2
+		radiusY = radiusY - 2
+	end
+	
+	if love.keyboard.isDown("left") then
+		radiusY = radiusY - 1
+	elseif love.keyboard.isDown("right") then
+		radiusY = radiusY + 1
+	end
+	
+	
 end
 
+function love.keyreleased(key)
+	if key == "space" then
+		--~ love.event.quit()
+		for i in pairs(ring) do
+			ring[i].angle = ring[i].angle + .1
+			ring[i].x = target.x + math.cos(ring[i].angle) * 100
+			ring[i].y = target.y + math.sin(ring[i].angle) * 100
+		end
+	end
+end
