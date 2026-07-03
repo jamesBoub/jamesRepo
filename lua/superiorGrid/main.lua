8e10e60e8f4550cb266ed8f8bc511811482768d7
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2026-07-03 00:33:36 -0700

    Update main.lua

--- lua/superiorGrid/main.lua
+++ lua/superiorGrid/main.lua
@@ -1,5 +1,8 @@
 grid = {}
-blocks = {{x = 1, y = 1}}
+blocks = {
+{{x = 1, y = 1},{x = 2, y = 1},{x = 3, y = 1}},
+{{x = 1, y = 4},{x = 2, y = 4},{x = 3, y = 4}}
+}
 offset = {x = 100, y = 0}
 
 for i = 1,40 do
@@ -12,17 +15,24 @@ for i = 1,40 do
 	offset.y = 0
 end
 
-grid[2][1].color = {1,1,1}
+--~ grid[2][1].color = {1,1,1}
+
+for i in pairs(blocks) do
+	for u in pairs(blocks[i]) do
+		grid[blocks[i][u].x][blocks[i][u].y].color = {1,1,1}
+	end
+end
 
 function love.draw()
 	for i in pairs(grid) do
 		for u in pairs(grid[i]) do
-		love.graphics.setColor(grid[i][u].color[1],grid[i][u].color[2],grid[i][u].color[3])
+		
+			love.graphics.setColor(grid[i][u].color[1],grid[i][u].color[2],grid[i][u].color[3])
+			
+			
+		
 		love.graphics.rectangle("fill", grid[i][u].x, grid[i][u].y, 9,9)
 		end
 	end
-	
-	
-	
 end
 
