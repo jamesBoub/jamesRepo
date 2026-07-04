2d5e860f8bf6595e0d12e4d0701043ba09737ed8
jamesBoub <145366258+jamesBoub@users.noreply.github.com>
2026-07-03 22:43:01 -0700

    Update main.lua

--- lua/derivativeBlockFallingGame/main.lua
+++ lua/derivativeBlockFallingGame/main.lua
@@ -1,97 +1,93 @@
-playerPos = {x = 1,y = 1}
+grid = {}
+blocks = {
+{{x = 3,y = 4},{x = 4,y = 4}},
+{{x = 3,y = 8},{x = 4,y = 8},{x = 5,y = 8}},
+}
+offset = {x = 0, y = 0}
 
-function love.load()
-	grid_create()
+selectedBlock = 2
+
+rows = 40
+cols = 40
+
+for i = 1,rows do
+	table.insert(grid, {})
+	for u = 1,cols do
+		table.insert(grid[i], {x = offset.x, y = offset.y, flags = {}})
+		offset.y = offset.y + 10
+	end
+	offset.x = offset.x + 10
+	offset.y = 0
+end
+
+function grid_blocks_check()
+	
+	for i in pairs(grid) do
+		for u in pairs(grid) do
+			grid[i][u].flags = {}
+		end
+	end
+	
+		for i in pairs(blocks) do
+			for u in pairs(blocks[i]) do
+				
+				if i == selectedBlock then
+					grid[blocks[i][u].x][blocks[i][u].y].flags[1] = "wall"
+				else
+					grid[blocks[i][u].x][blocks[i][u].y].flags[1] = "wall"
+				end
+				
+			end
+		end
+end
+
+grid_blocks_check()
+
+function love.draw()
+	for i in pairs(grid) do
+		for u in pairs(grid[i]) do
+			love.graphics.setColor(1,1,1)
+			if grid[i][u].flags[1] == "wall" then
+				love.graphics.setColor(1,0,0)
+			end
+			love.graphics.rectangle("fill", grid[i][u].x, grid[i][u].y, 9,9)
+		end
+	end
 end
 
 function love.update()
-	player()
-	input()
+	--~ for i in pairs(blocks) do
+		--~ for u in pairs(blocks) do
+			
+		--~ end
+	--~ end
 end
 
-function love.draw()
-	draw_grid()
-	grid_manage()
-	love.graphics.print(playerPos.x .. " " .. playerPos.y, 100, 600)
-end
 
-function draw_grid()
-	for i in pairs(grid) do
-		for u in pairs(grid[i]) do
-		love.graphics.setColor(grid[i][u].color[1],grid[i][u].color[2],grid[i][u].color[3])
-		love.graphics.rectangle("fill", grid[i][u].x, grid[i][u].y, cellDimensions,cellDimensions)
-		end
-	end
-	love.graphics.setColor(1,1,1) -- reset color for next frame
-end
-
-function grid_create()
-	grid = {}
-	offset = {x = 10, y = 0}
-	cellDimensions = 10
-	cellDistance = cellDimensions
-
-	rows = 40
-	columns = 59
-
-	for i = 1,rows do
-		table.insert(grid, {})
-		for u = 1,columns do
-			table.insert(grid[i], {x = offset.x, y = offset.y, color = {1,1,1}})
-			offset.y = offset.y + cellDistance
-		end
-		offset.x = offset.x + cellDistance
-		offset.y = 0
-	end
-end
-
-function player()
-end
-
-function input()
-
-
---~ mouse_grid_collisions()
 
 function love.keyreleased(key)
-	if key == "escape" then
-		love.event.quit()
-	end
-	if key == "w" then
-		playerPos.y = playerPos.y - 1
+	if key == "d" then
+		for i in pairs(blocks[selectedBlock]) do
+			blocks[selectedBlock][i].x = blocks[selectedBlock][i].x + 1
+		end
+		grid_blocks_check()
 	end
 	if key == "a" then
-		playerPos.x = playerPos.x - 1
+		for i in pairs(blocks[selectedBlock]) do
+			blocks[selectedBlock][i].x = blocks[selectedBlock][i].x - 1
+		end
+		grid_blocks_check()
+	end
+	if key == "w" then
+		for i in pairs(blocks[selectedBlock]) do
+			blocks[selectedBlock][i].y = blocks[selectedBlock][i].y - 1
+		end
+		grid_blocks_check()
 	end
 	if key == "s" then
-		playerPos.y = playerPos.y + 1
-	end
-	if key == "d" then
-		playerPos.x = playerPos.x + 1
-	end
-	end
-end
-
-function mouse_grid_collisions()
-	mouseX,mouseY = love.mouse.getPosition()
-	for i in pairs(grid) do
-		for u in pairs(grid[i]) do
-			if mouseX > grid[i][u].x and mouseX < grid[i][u].x + cellDimensions + 1 and mouseY < grid[i][u].y then
-				grid[i][u].color = {0,0,1}
-			end
+		for i in pairs(blocks[selectedBlock]) do
+			blocks[selectedBlock][i].y = blocks[selectedBlock][i].y + 1
 		end
+		grid_blocks_check()
 	end
 end
-
-function grid_manage()
-	for i in pairs(grid) do
-		for u in pairs(grid[i]) do
-			if playerPos.x == i and playerPos.y == u then
-				grid[i][u].color = {1,0,0}
-			else
-				grid[i][u].color = {1,1,1}
-			end
-		end
-	end
-end
-
